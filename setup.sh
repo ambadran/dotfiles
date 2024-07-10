410868f6327d8eb60a208c49dd4e9c8c9811043a
Abdulrahman Badran <88182204+ambadran@users.noreply.github.com>
2024-07-10 03:46:16 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 sudo apt install vim
 sudo apt install vim-gtk3
+sudo apt install tmux
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
