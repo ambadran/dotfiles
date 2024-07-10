c12e363a8db17f3e925e358053cb445680bb21c6
Abdulrahman Badran <88182204+ambadran@users.noreply.github.com>
2024-07-10 03:49:08 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ sudo apt install tmux
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# now pending install vim libraries by opening vim and typing :PluginInstall
+# also install tmux libraries by opening tmux, escape character then :source ~/.tmux.conf
