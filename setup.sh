ef69df10099ca0f96300ca2cb8b07117e1101d60
Abdulrahman Badran <88182204+ambadran@users.noreply.github.com>
2024-10-08 11:06:39 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 
-
+sudo apt update
 sudo apt install vim
-sudo apt install vim-gtk3 
+sudo apt install vim-gtk3
+sudo apt install tmux
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-cp .bashrc ..
-cp .bashprofile ..
-cp .vimrc ..
+# now pending install vim libraries by opening vim and typing :PluginInstall
+# also install tmux libraries by opening tmux, escape character then :source ~/.tmux.conf
 
+cp .bashprofile ~/
+cp .bashrc ~/
+cp .vimrc ~/
+cp .tmux.conf ~/
+
+tmux source ~/.tmux.conf
