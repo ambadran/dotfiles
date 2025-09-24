0f62770098f1b93e54e39092c5d0111d797ab350
ambadran <ambadran717@hotmail.com>
2025-09-24 13:23:16 +0300

    Added latest linux dotfiles

--- setup.sh
+++ setup.sh
@@ -15,3 +15,16 @@ tmux source ~/.tmux.conf
 # User must now install
 # Vim plugins: vim -> :PluginInstall
 # Tmux plugins: tmux <key> + I
+
+
+# Installing Pyenv
+curl -fsSL https://pyenv.run | bash
+# Need to refresh to apply pyenv
+exec "$SHELL"
+
+# Installing Pyenv virtualenv plugin
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+# Need to refresh to apply pyenv
+exec "$SHELL"
+
+
