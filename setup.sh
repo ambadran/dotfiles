73a6f0b8385ab216543cee677eb886b877027265
ambadran <ambadran717@hotmail.com>
2026-08-06 16:21:19 +0300

    Automated update of linux dotfiles

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Updating APT packages..."
 sudo apt update
 
 # Install core packages needed everywhere
-sudo apt install -y tmux fzf bat tree direnv picocom git curl btop nvtop
+sudo apt install -y tmux fzf bat tree direnv picocom git curl btop nvtop duf gdu
 
 echo "Installing Neovim..."
 
